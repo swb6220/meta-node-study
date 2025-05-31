f3c21f0d270eef47c57e2a0e0d4131506b717b9f
ShiWenbo <swb6220@126.com>
2025-05-31 10:09:32 +0800

    task2 work3 - begging contracts

---
+++ solidity/task2/work3/contracts/BeggingContract.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/timestamp"
+
+contract BeggingContract {
+
+    mapping(address user => uint256 amount) donateAmounts;
+    mapping(address user => string message) donateMessage;
+
+    DonateRank[3] private donateRanks;
+    uint256 private donateStartTime;
+    uint256 private donatePeriod;
+    address private owner;
+
+    modifier ownerable {
+        require(msg.sender == owner, "The caller should be the conctract owner.");
+        _;
+    }
+
+    event Donation(address indexed donateUser, string message);
+
+    struct DonateRank {
+        address donateUser;
+        uint256 donateAmount;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable { }
+
+    function setDonateTime(uint256 startTime, uint256 period) public ownerable {
+        require(msg.sender == owner, "The caller should be the conctract owner.");
+        require(startTime >= block.timestamp && (period > 0), "Illegal parameters for setDonateTime");
+         donateStartTime = startTime;
+         donatePeriod = period;
+    }
+
+    function getDonteTime() public view returns (string startTime, string endTime) {
+        TimestampConverter converter = new TimestampConverter(donateStartTime, donatePeriod);
+        (string memory start, string memory end) = converter.getTimeStrings();
+        return (start, end);
+    }
+
+    function _setRanks(address user, uint256 amount) internal {
+        if(amount > donateRanks[0].donateAmount) {
+            _updateRank(donateRanks[2], donateRanks[1].donateUser, donateRanks[1].donateAmount);
+            _updateRank(donateRanks[1], donateRanks[0].donateUser, donateRanks[0].donateAmount);
+            _updateRank(donateRanks[0], user, amount);
+        } else if(amount > donateRanks[1].donateAmount) {
+            _updateRank(donateRanks[2], donateRanks[1].donateUser, donateRanks[1].donateAmount);
+            _updateRank(donateRanks[1], user, amount);
+        } else if(amount > donateRanks[2].donateAmount) {
+            _updateRank(donateRanks[2], user, amount);
+        }
+    }
+
+    function _updateRank(DonateRank storage rank, address user, uint256 amount) internal {
+        rank.donateAmount = amount;
+        rank.donateUser = user;
+    }
+
+    function donate(string memory message) public payable {
+        require(msg.value > 0, "The donated amount must be greater than zero.");
+        donateAmounts[msg.sender] += msg.value;
+        if(bytes(message).length > 0) {
+            donateMessage[msg.sender] = message;
+        }
+
+        _setRanks(msg.sender, msg.value);
+
+        emit Donation(msg.sender, message);
+    }
+
+    function getDonatedAmount(address user) public view returns (uint256) {
+         return donateAmounts[user];
+    }
+
+    function withdraw() public ownerable{
+        payable(owner).transfer(address(this).balance);
+    }
+
+    function getDonateRanks() public view returns (address[3] memory) {
+        return [donateRanks[0].donateUser, donateRanks[1].donateUser, donateRanks[2].donateUser];
+    }
+
+}
---
+++ solidity/task2/work3/tests/TestResult.md
@@ -0,0 +1,26 @@
+# BeggingContract测试结果
+
+## 合约地址
+0x7feB4C4E20b9930D8cF56A5af7Ae4Dd75C067acc
+
+## 测试结果
+### donate
+![](.\img\donate1.png)  
+![](.\img\donate2.png)  
+### 捐献排名
+![](.\img\getRanks.png)  
+### 设置捐献时间
+![](.\img\setDonateTime1_1.png)   
+![](.\img\setDonateTime1-2.png)  
+![](.\img\setDonateTime2-1.png)  
+![](.\img\setDonateTime2-2.png)  
+![](.\img\setDonateTime2-3.png)  
+![](.\img\setDonateTime2-4.png)  
+![](.\img\setDonateTime2-5.png)  
+### 提款
+![](.\img\withdraw1-1.png)  
+![](.\img\withdraw2-1.png)
+![](.\img\withdraw2-2.png)
+
+
+
